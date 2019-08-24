561f261baba26bef069b2a0f4f3848b1475648f8
pravash-upreti <pravashupreti@gmail.com>
2019-08-24 11:07:14 +0545

    Export the model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,2 +1,7 @@
 import api.models.monkey_patch
+
+from api.models.comment import Comment
+from api.models.friend import Friend
+from api.models.like import Like
+from api.models.post import Post
 from api.models.user import User

8d2c3df20f19218fa67cd2a0aea6d7f96f3be115
pravash-upreti <13918230+pravash-upreti@users.noreply.github.com>
2019-08-27 15:19:39 +0545

    Add Auth service and exception

--- api/exceptions/__init__.py
+++ api/exceptions/__init__.py
@@ -2,3 +2,4 @@ from api.exceptions.generic_exception import GenericException
 from api.exceptions.invalid_payload_exception import InvalidPayloadException
 from api.exceptions.row_not_found_exception import RowNotFoundException
 from api.exceptions.file_operation_exception import FileOperationException
+from api.exceptions.auth_exception import AuthException
